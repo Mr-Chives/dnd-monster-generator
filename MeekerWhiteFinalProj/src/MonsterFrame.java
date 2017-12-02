a35201e9e6db4433115a8c06f18ebbb056c6b3ca
Gutzblackswordsman <31457938+Gutzblackswordsman@users.noreply.github.com>
2017-12-02 15:42:30 -0600

    Add files via upload
    Creates the ui and adds the menu along with image and statistics. currently need to work on sizing of the image

---
+++ MeekerWhiteFinalProj/src/MonsterFrame.java
@@ -0,0 +1,92 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+
+public class MonsterFrame extends JFrame {
+	public MonsterFrame() {
+		setBounds(100,100,1000,805);
+		setTitle("D&D Monster Generator V0.101");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setIconImage(new ImageIcon(getClass().getResource("beholder.jpg")).getImage());
+		Container c = getContentPane();
+		c.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		//might need to add if(shouldFill)
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		JPanel imagePan = new JPanel();
+		ImageIcon monster = new ImageIcon(".\\beholder.jpg");
+		imagePan.add(new JLabel(monster));
+		imagePan.setBorder(BorderFactory.createLoweredBevelBorder());
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.ipadx = 300;
+		gbc.ipady = 500;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 2;
+		gbc.weighty = 0.5;
+		c.add(imagePan, gbc);
+		JPanel menuPan = new JPanel();
+		menuPan.setBorder(BorderFactory.createLoweredBevelBorder());
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.ipadx = 300;
+		gbc.ipady = 300;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 2;
+		c.add(menuPan, gbc);
+		JPanel statPan = new JPanel();
+		statPan.setBorder(BorderFactory.createLoweredBevelBorder());
+		statPan.setBackground(Color.GRAY);
+		statPan.setOpaque(true);
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		gbc.ipadx = 700;
+		gbc.ipady = 250;
+		gbc.gridwidth = 2;
+		gbc.gridheight = 1;
+		c.add(statPan, gbc);
+		JPanel descPan = new JPanel();
+		descPan.setBorder(BorderFactory.createLoweredBevelBorder());
+		descPan.setBackground(Color.GRAY);
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		gbc.ipadx = 700;
+		gbc.ipady = 650;
+		gbc.gridwidth = 2;
+		gbc.gridheight = 3;
+		c.add(descPan, gbc);
+		menuPan.setLayout(new GridLayout(3,1));
+		JButton btnNew = new JButton("New Monster");
+		btnNew.setBackground(Color.GRAY);
+		btnNew.setForeground(Color.WHITE);
+		btnNew.setFont(new Font("Gabriola",Font.BOLD,30));
+		btnNew.setFocusPainted(false);
+		menuPan.add(btnNew);
+		JButton btnSave = new JButton("Save Monster");
+		btnSave.setBackground(Color.GRAY);
+		btnSave.setForeground(Color.WHITE);
+		btnSave.setFont(new Font("Gabriola",Font.BOLD,30));
+		menuPan.add(btnSave);
+		JButton btnLoad = new JButton("Load In Monster");
+		btnLoad.setBackground(Color.GRAY);
+		btnLoad.setForeground(Color.WHITE);
+		btnLoad.setFont(new Font("Gabriola",Font.BOLD,30));
+		menuPan.add(btnLoad);
+	}
+	public static void main(String[] args) {
+		MonsterFrame frm = new MonsterFrame();
+		frm.setVisible(true);
+	}
+}
